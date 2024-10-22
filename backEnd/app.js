3af020015025c6d90aa0dde9025602e6661315f8
Mohammed <arifcsidhik@gmail.com>
2024-10-22 12:08:34 +0530

    added custom error handlers

--- backEnd/app.js
+++ backEnd/app.js
@@ -5,18 +5,28 @@ import cors from "cors";
 import logger from "morgan";
 import authRouter from "./routes/authRoutes.js";
 import adminRouter from "./routes/adminRoutes.js"
+import globalErrorHandler from "./error/globalErrorHandler.js";
+import { AppError }  from "./utils/appError.js";
 
 const app = express();
 
 // app.use(cors(*));
 // app.use(cookieParser());
-// app.use(express.json({limit: "15kb"}));        //------- Middleware to parse JSON requests with a limit ----- ---
+// app.use(express.json({limit: "10kb"}));        //------- Middleware to parse JSON requests with a limit ----- ---
 app.use(logger('tiny'));
-
 
 
 app.use("/api/auth", authRouter);
 app.use("/api/admin", adminRouter);
+
+// Handle all undefined routes
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+
+// global error handler middleware
+app.use(globalErrorHandler);
 
 
 export default app; 
--- backEnd/routes/authRoutes.js
+++ backEnd/routes/authRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
+import { catchAsync } from "../error/catchAsync.js";
 const router = express.Router();
 
 
 
 
+router.get("/test",catchAsync(async(req,res)=>{
 
+    const user = "arif"
+    user ? res.status(200).json({status:"sucess",user}) : res.status(500).json({status:"error",message:"user not found"})
+    
+}));
 
 
 
